f461ea945a31d9690ba16fcb81d3a23a6861ef9f
yksIsky <103578352+yksIsky@users.noreply.github.com>
2022-06-04 16:26:16 +0200

    Update Test atos.py

--- Test atos.py
+++ Test atos.py
@@ -1,63 +1,20 @@
-# Python 3.1
-from machine import Pin
-from time import sleep
+import random
 
 
-class Pic_morse():
-    def __init__(self):
-        self.MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
-                                'C': '-.-.', 'D': '-..', 'E': '.',
-                                'F': '..-.', 'G': '--.', 'H': '....',
-                                'I': '..', 'J': '.---', 'K': '-.-',
-                                'L': '.-..', 'M': '--', 'N': '-.',
-                                'O': '---', 'P': '.--.', 'Q': '--.-',
-                                'R': '.-.', 'S': '...', 'T': '-',
-                                'U': '..-', 'V': '...-', 'W': '.--',
-                                'X': '-..-', 'Y': '-.--', 'Z': '--..',
-                                '1': '.----', '2': '..---', '3': '...--',
-                                '4': '....-', '5': '.....', '6': '-....',
-                                '7': '--...', '8': '---..', '9': '----.',
-                                '0': '-----', ', ': '--..--', '.': '.-.-.-',
-                                '?': '..--..', '/': '-..-.', '-': '-....-',
-                                '(': '-.--.', ')': '-.--.-'}
+def warm_cold():
+    ran_int = random.randint(1,51)
+    
+    ip = 0
+    while ran_int != ip:
+        ip = int(input("Provide number from 1 -50 "))
+        if ran_int == -1:
+            print("End Game")
+            break
+        elif ip > ran_int:
+            print("cold")
+        elif ip < ran_int:
+            print("warm")
+            
+    return print("You Won")
 
-    def pin_nr(self):
-
-        pin_nr = int(input("Pin number from 1-25 default 25: "))
-        self.builtin_led = Pin(pin_nr, Pin.OUT)
-
-    def morse_name(self):
-        name = input("Provide your name: ")
-        name = name.replace(" ", "")
-        morse_name = []
-        for i in name:
-            morse_name.append(self.MORSE_CODE_DICT.get(i.upper()))
-        return [i for i in morse_name]
-
-    def long(self):
-        self.builtin_led.value(1)
-        sleep(0.6)
-        self.builtin_led.value(0)
-        sleep(0.6)
-
-    def short(self):
-        self.builtin_led.value(1)
-        sleep(0.2)
-        self.builtin_led.value(0)
-        sleep(0.2)
-
-    def morse_code(self, ):
-        self.pin_nr()
-        m_name = self.morse_name()
-        for i in m_name:
-            for l in i:
-                if l == "-":
-                    self.long()
-                elif l == ".":
-                    self.short()
-            sleep(1)
-
-
-if __name__ == "__maine__":
-    new_name = Pic_morse()
-    new_name.morse_code()
+warm_cold()
